286172f8639aee14e1c8d2b449392b9a7c6ee33b
AbhisekGhosh1988 <ghosh.abhisek1988@gmail.com>
2022-09-04 22:02:27 +0530

    Update HelloWorldApplicationTests.java

--- src/test/java/com/abhisek/demo/HelloWorldApplicationTests.java
+++ src/test/java/com/abhisek/demo/HelloWorldApplicationTests.java
@@ -2,12 +2,38 @@ package com.abhisek.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class HelloWorldApplicationTests {
-
+@Autowired
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void messageTest() throws Exception {
+
+		mockMvc.perform(get("/hello")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testMain() {
+		HelloWorldApplication.main(new String[] {});
+		assertThat(true);
 	}
 
 }
