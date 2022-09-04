d1791cbf0fc4a2711d645d97d15b590321e023f3
AbhisekGhosh1988 <ghosh.abhisek1988@gmail.com>
2022-09-04 22:16:02 +0530

    Update HelloWorldApplicationTests.java

--- src/test/java/com/abhisek/demo/HelloWorldApplicationTests.java
+++ src/test/java/com/abhisek/demo/HelloWorldApplicationTests.java
@@ -1,15 +1,34 @@
 package com.abhisek.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
 class HelloWorldApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
 	}
 	
+	@Test
+	public void messageTest() throws Exception {
 
+		mockMvc.perform(get("/hello")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testMain() {
+		HelloWorldApplication.main(new String[] {});
+		assertThat(true);
+	}
 
 }
+
